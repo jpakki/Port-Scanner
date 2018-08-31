43181c57b4c1042c2fac888c8e0f5781cc76aa2a
Jaswant Pakki <jpakki1@asu.edu>
2018-08-30 18:31:01 -0700

    connScan and portScan functions added; gitignore added

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import argparse
+from socket import *
 
 parser = argparse.ArgumentParser(usage='-H <target host> -p <target port>')
+
 parser.add_argument('-H', dest='tgtHost', type=str, help='specify target host')
 parser.add_argument('-p', dest='tgtPort', type=int, help='specify target port')
 args = parser.parse_args()
@@ -8,6 +10,33 @@ args = parser.parse_args()
 tgtHost = args.tgtHost
 tgtPort = args.tgtPort
 
-if (tgtHost == None)| (tgtPort == None): 
+if (tgtHost == None)|(tgtPort == None): 
     print parser.usage
-#print args.tgtHost
+    exit(0)
+
+def connScan(tgtHost, tgtPort): 
+    try: 
+        connSkt = socket(AF_INET, SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        print '[+]%d/tcp open' + tgtPort
+        connSkt.close()
+    except: 
+        print '[-]%d/tcp closed' + tgtPort
+
+def portScan(tgtHost, tgtPorts): 
+    try: 
+        tgtIP = gethostbyname(tgtHost)
+    except: 
+        print "[-]Cannot resolve '%s': Unknown host"% tgtHost
+        return
+
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print '\n[+] Scan results for: ' + tgtName[0]
+    except: 
+        print '\n[+] Scan results for: ' + tgtIP
+    setdefaulttimeout(1)
+    
+    for tgtPort in tgtPorts:
+        print 'Scanning port ' + tgtPort
+        connScan(tgtHost, int(tgtPort))
