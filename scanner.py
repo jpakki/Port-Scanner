cc7f741d4faba34e379c6b4b39f07980df3ef683
Jaswant Pakki <jpakki1@asu.edu>
2018-09-02 17:07:10 -0700

    added nmap scanning

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import argparse
+import nmap
 from socket import *
 
 def connScan(tgtHost, tgtPort): 
@@ -7,29 +8,37 @@ def connScan(tgtHost, tgtPort):
         connSkt.connect((tgtHost, tgtPort))
         connSkt.send('ViolentPython\r\n')
         results = connSkt.recv(100)
-        print '[+]%d/tcp open'% tgtPort
-        print '[+] ' + str(results)
+        print('[+]%d/tcp open'% tgtPort)
+        print('[+] ' + str(results))
         connSkt.close()
     except: 
-        print '[-]%d/tcp closed'% tgtPort
+        print('[-]%d/tcp closed'% tgtPort)
 
 def portScan(tgtHost, tgtPorts): 
     try: 
         tgtIP = gethostbyname(tgtHost)
     except: 
-        print "[-]Cannot resolve '%s': Unknown host"% tgtHost
+        print("[-]Cannot resolve '%s': Unknown host"% tgtHost)
         return
 
     try:
         tgtName = gethostbyaddr(tgtIP)
-        print '\n[+] Scan results for: ' + tgtName[0]
+        print('\n[+] Scan results for: ' + tgtName[0])
     except: 
-        print '\n[+] Scan results for: ' + tgtIP
+        print('\n[+] Scan results for: ' + tgtIP)
     setdefaulttimeout(1)
 
     for tgtPort in tgtPorts:
-        print 'Scanning port ' + tgtPort
+        # t = thread.start_new_thread(connScan, (tgtHost, int(tgtPort)))
+        print('Scanning port ' + tgtPort)
         connScan(tgtHost, int(tgtPort))
+
+def nmapScan(tgtHost, tgtPort):
+    nmscan = nmap.PortScanner()
+    nmscan.scan(tgtHost, tgtPort)
+    state = nmscan[tgtHost]['tcp'][int(tgtPort)]['state']
+    print(' [*] ' + tgtHost + ' tcp/' + tgtPort + ' ' + state)
+
 
 def main(): 
     parser = argparse.ArgumentParser(usage='-H <target host> -p <target port>')
@@ -42,8 +51,10 @@ def main():
     tgtPorts = str(args.tgtPort).split(', ')
 
     if (tgtHost == None)|(tgtPorts[0] == None): 
-        print parser.usage
+        print(parser.usage)
         exit(0)
-    portScan(tgtHost, tgtPorts)
+    for tgtPort in tgtPorts:
+        nmapScan(tgtHost, tgtPort)
+        #portScan(tgtHost, tgtPorts)
 if __name__ == '__main__':
     main()
